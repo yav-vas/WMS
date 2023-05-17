1901fafaf600bde2dc8b1d7e90be065bf184a59f
TE55DATH <124620873+TE55DATH@users.noreply.github.com>
2023-05-17 21:27:19 +0300

    Add files via upload
    Should be okay now

--- WMS/src/views/SalesRepresentative.java
+++ WMS/src/views/SalesRepresentative.java
@@ -1,5 +1,37 @@
 package views;
 
-public class SalesRepresentative {
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
+public class SalesRepresentative extends JFrame  {
+	private JTextField txtOrders;
+	public SalesRepresentative() {
+		getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("New Order");
+		btnNewButton.setBounds(180, 20, 233, 193);
+		getContentPane().add(btnNewButton);
+		
+		txtOrders = new JTextField();
+		txtOrders.setText("    Orders:");
+		txtOrders.setBounds(10, 0, 116, 19);
+		getContentPane().add(txtOrders);
+		txtOrders.setColumns(10);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(133, 20, 17, 233);
+		getContentPane().add(scrollBar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 20, 121, 233);
+		getContentPane().add(scrollPane);
+	}
+	private static class __Tmp {
+		private static void __tmp() {
+			  javax.swing.JPanel __wbp_panel = new javax.swing.JPanel();
+		}
+	}
 }
